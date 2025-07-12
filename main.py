98a5e79724f04be5e72c7f63d18de9397af11259
Filip Gregor <filip.gregor@gmail.com>
2025-07-12 20:19:47 +0200

    added sys.argv to support multiple inputs

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from stats import get_num_words, count_characters
+import sys
 
 def main():
-    path = "books/frankenstein.txt"
-    num_words = (get_num_words(path))
-    # report output
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    char_dic = count_characters(path)
-    for entry in char_dic:
-        char = entry['char']
-        value = entry['num']
-        if char.isalpha():
-            print(f"{char}: {value}")
-
-
+    if(len(sys.argv) < 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    for path in sys.argv[1:]:
+        num_words = (get_num_words(path))
+        # report output
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        char_dic = count_characters(path)
+        for entry in char_dic:
+            char = entry['char']
+            value = entry['num']
+            if char.isalpha():
+                print(f"{char}: {value}")
 
 main()
