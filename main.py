792e9568a4317c3e1a738b4691b6116bbaa1da46
Filip Gregor <filip.gregor@gmail.com>
2025-07-12 11:10:25 +0200

    counting words with report print

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
+def word_count(text):
+    return len(text.split())
+
 def main():
     path = "books/frankenstein.txt"
-    print(get_book_text(path))
+    num_words = (word_count(get_book_text(path)))
+    print(f"{num_words} words found in the document")
+
 
 main()
