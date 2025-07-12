17b353f581601843c4a57a51a781144e7bb19fb2
Filip Gregor <filip.gregor@gmail.com>
2025-07-12 20:06:19 +0200

    report

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ from stats import get_num_words, count_characters
 def main():
     path = "books/frankenstein.txt"
     num_words = (get_num_words(path))
-    print(f"{num_words} words found in the document")
+    # report output
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    char_dic = count_characters(path)
+    for entry in char_dic:
+        char = entry['char']
+        value = entry['num']
+        if char.isalpha():
+            print(f"{char}: {value}")
 
-    count_characters(path)
 
 
 main()
--- stats.py
+++ stats.py
@@ -6,6 +6,9 @@ def get_num_words(file_path):
     text = get_text(file_path)
     return len(text.split())
 
+def sort_on(items):
+    return items["num"]
+
 def count_characters(file_path):
     text = get_text(file_path)
     text = text.lower()
@@ -17,5 +20,8 @@ def count_characters(file_path):
         else:
             char_dic[c] = char_dic[c] + 1
 
-    for entry in char_dic:
-        print(f"'{entry}': {char_dic[entry]}")
+    result = [{'char': k, 'num': v} for k, v in char_dic.items()]
+
+    result.sort(reverse=True, key=sort_on)
+
+    return result
